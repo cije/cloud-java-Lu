9b8aac7a1ed82eb4b6ab20c4f69397e865c1750f
cije <an37500@163.com>
2021-04-13 16:23:16 +0800

    Ribbon负载均衡

--- cloud-order/src/main/java/com/ce/cloudorder/CloudOrderApplication.java
+++ cloud-order/src/main/java/com/ce/cloudorder/CloudOrderApplication.java
@@ -3,6 +3,7 @@ package com.ce.cloudorder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,6 +11,10 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class CloudOrderApplication {
 
+    /**
+     * `@LoadBalanced`注解 声明当前的RestTemplate具有ribbon负载均衡的能力
+     */
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
--- cloud-order/src/main/java/com/ce/cloudorder/controller/OrderController.java
+++ cloud-order/src/main/java/com/ce/cloudorder/controller/OrderController.java
@@ -15,9 +15,20 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
+    /**
+     * Ribbon负载均衡实现的流程
+     * <ol>
+     * <li>RestTemplate发送的请求是服务名称http://nacos-product/product/getProductById/1</li>
+     * <li>因为RestTemplate上加了 注解@LoadBalanced</li>
+     * <li>所以Ribbon会拦截 RestTemplate发送的请求</li>
+     * <li>根据url中的服务名称 以及自身的负载均衡策略 去订单服务的服务列表中找到一个要调用的ip+端口号 localhost:8802</li>
+     * <li>然后修改url为 http://localhost:8802/product/getProductById/1</li>
+     * <li>将新的请求再次发送</li>
+     * </ol>
+     */
     @GetMapping("/order/getOrderInfo")
     public String getOrderInfo() {
-        String url = "http://localhost:8801/product/getProductById/1";
+        String url = "http://nacos-product/product/getProductById/1";
         String msg = restTemplate.getForObject(url, String.class);
         return "查询订单成功，" + msg;
     }
